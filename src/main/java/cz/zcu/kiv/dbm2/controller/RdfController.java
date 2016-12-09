462a9c2decab6facbc3f08708ae96c8a2f78f1cc
Petr Dobřička <Petr Dobika>
2016-12-09 19:51:22 +0100

    membrove tridy"

--- src/main/java/cz/zcu/kiv/dbm2/controller/RdfController.java
+++ src/main/java/cz/zcu/kiv/dbm2/controller/RdfController.java
@@ -17,6 +17,13 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.Resource;
 
 /**
  * Created by Matej Lochman on 14.11.16.
@@ -66,9 +73,34 @@ public class RdfController {
         System.out.println("Properties of node " + nodeId);
         Map<RDFNode, List<RDFNode>> properties;
         properties = rdfService.getNodeProperties(rdfModel, nodeId);
+        String type = "";
         for (Map.Entry<RDFNode, List<RDFNode>> entry : properties.entrySet()) {
             System.out.println("values of: " + entry.getKey().toString() + ":");
+            if ("http://www.w3.org/1999/02/22-rdf-syntax-ns#type".equals(entry.getKey().toString())){
+                type = entry.getValue().toString().substring(1, entry.getValue().toString().length()-1) ;
+            }
             System.out.println("\t" + entry.getValue().toString());
+        }
+        String queryString = "prefix :      <http://example.org/>\n" +
+            "prefix rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+            "prefix owl:   <http://www.w3.org/2002/07/owl#>\n" +
+            "prefix xsd:   <http://www.w3.org/2001/XMLSchema#>\n" +
+            "prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#>\n" +
+            "select ?p ?d where {"+
+            "?p rdfs:domain/(owl:unionOf/rdf:rest*/rdf:first)* ?d\n"+
+            "filter (isIri(?d) && ?d =  <"+type+"> )\n"+
+            "}" ;
+        System.out.println("wtf proc se nic nedeje" + type);
+        Query query = QueryFactory.create(queryString) ;
+        try (QueryExecution qexec = QueryExecutionFactory.create(query, ontModel)) {
+            ResultSet results = qexec.execSelect() ;
+            for ( ; results.hasNext() ; )
+            {
+            QuerySolution soln = results.nextSolution() ;
+                System.out.println("je ve tridach a typech : "+soln.get("p"));       // Get a result variable by name.
+          //  Resource r = soln.getResource("VarR") ; // Get a result variable - must be a resource
+          //  Literal l = soln.getLiteral("VarL") ;   // Get a result variable - must be a literal
+            }
         }
         return "node";
     }
